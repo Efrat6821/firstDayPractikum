5bc1909146cc0016f8357009e4b0b56a299effdf
Efrat6821 <Efrat6821+g@gmail.com>
2024-07-03 13:36:20 +0300

    add validate User Details

--- models/user.model.js
+++ models/user.model.js
@@ -1,43 +1,52 @@
 class User {
     constructor(id, name, email, phone) {
-      this.id = id;
-      this.name = name;
-      this.email = email;
-      this.phone = phone;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
     }
-  }
-  
-  class UserManager {
+}
+
+class UserManager {
     constructor() {
-      this.users = new Map();
-      this.currentId = 1;
+        this.users = new Map();
+        this.currentId = 1;
     }
-  
+
+    validateUserDetails(name, email, phone) {
+        if (!name || !email || !phone) {
+            throw new Error("All fields (name, email, phone) are required");
+        }
+        if (/\s/.test(email) || !email.includes("@")) {
+            throw new Error("Invalid email format");
+        }
+    }
     createUser(name, email, phone) {
-      const user = new User(this.currentId++, name, email, phone);
-      this.users.set(user.id, user);
-      return user;
+        this.validateUserDetails(name, email, phone);
+        const user = new User(this.currentId++, name, email, phone);
+        this.users.set(user.id, user);
+        return user;
     }
-  
+
     updateUser(id, name, email, phone) {
-      if (!this.users.has(id)) {
-        return null;
-      }
-      const user = this.users.get(id);
-      user.name = name;
-      user.email = email;
-      user.phone = phone;
-      return user;
+        if (!this.users.has(id)) {
+            return null;
+        }
+        this.validateUserDetails(name, email, phone); 
+        const user = this.users.get(id);
+        user.name = name;
+        user.email = email;
+        user.phone = phone;
+        return user;
     }
-  
+
     deleteUser(id) {
-      return this.users.delete(id);
+        return this.users.delete(id);
     }
-  
+
     getUserById(id) {
-      return this.users.get(id) || null;
+        return this.users.get(id) || null;
     }
-  }
-  
-  module.exports = new UserManager();
-  
+}
+
+module.exports = new UserManager();
